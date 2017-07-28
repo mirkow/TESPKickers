c8eac9652c5c53ffd8598f03fbf6c7beb10c02fa
Mirko Waechter <mirko.waechter@kit.edu>
2017-07-28 09:26:11 +0200

    added python3 virtual env + sample file

---
+++ python3/NXTsample.py
@@ -0,0 +1,48 @@
+import nxt
+from nxt.motor import *
+import time
+import bluetooth
+def playing_around():
+    #nxt.locator.make_config()
+    brick = nxt.find_one_brick(None, None, False, False, True)
+    # brick.mc.start()
+    motor = nxt.Motor(brick, PORT_A)
+    motorB = nxt.Motor(brick, PORT_B)
+    print("before: " + str(motor.get_tacho()))
+
+    # brick.
+    # brick.mc.move_to(PORT_A, 100,30000)
+    # brick.mc.move_to(PORT_B, 100,50)
+    print("after: " + str(motor.get_tacho()))
+
+    motorB.run(70)
+    time.sleep(1)
+    motorB.run(-127)
+    time.sleep(0.2)
+    motorB.run(0)
+
+# return
+    # brick.
+    # brick.play_tone_and_wait(100,1000)
+    # brick.mc.start()
+    # time.sleep(1)
+    # motor.run(100)
+    # # motor.turn(127, 10000, False)
+    # for i in range(0,10):
+    #     time.sleep(1)
+    #     print(motor.get_tacho())
+    # motor.brake()
+    # for i in range(5,8):
+    #
+    #     time.sleep(0.4)
+    #     print(motor.get_tacho())
+    # motor.run(0)
+
+    # brick.mc.cmd(PORT_A, 10, 100)
+    # for i in range(0,10):
+    #     # time.sleep(0.1)
+    #     print(motor.get_tacho())
+    # motor.brake()
+    print("after: " + str(motor.get_tacho()))
+
+playing_around()
